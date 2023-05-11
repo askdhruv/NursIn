2133c56c32d8b0bd6e1c358803c2906fbc022ada
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2023-05-11 22:45:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ menubar.onclick = () =>{
     menubar.classList.toggle('fa-times')
     mynav.classList.toggle('active');
 }
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000, 
+      pause: 'hover' 
+    });
+  });
